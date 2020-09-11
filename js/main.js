6809a8640e5559769d01a19d32a5ad4745d1b10d
Francisco Stanley <Franciscothestanley@gmail.com>
2020-09-11 13:35:03 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    speed: 350,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    autoplayspeed: 3000,
+    adaptiveHeight: true
+});
