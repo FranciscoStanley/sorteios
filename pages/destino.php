4166530bdc8a1aa2fa1ba10b0d902bec8337dc0a
Francisco Stanley <Franciscothestanley@gmail.com>
2020-09-19 13:08:22 -0300

    Update destino.php

--- pages/destino.php
+++ pages/destino.php
@@ -22,14 +22,13 @@
     $content = new SendGrid\Content("text/html", "Ola, <br><br> PEGO COM SUCESSO! <br><br> Nome: $nome<br> Nome: $nome<br> Email: $email<br> Celular: $celular<br> Endereco: $endereco<br> Bairro: $bairro<br>");
     $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-    $apiKey = gatenv('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');
+    $apiKey = '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';
     $ag = new \SendGrid($apiKey);
 
     $response = $sg->cliente->mail()->send()->post($mail);
 
-    echo $response->statusCode();
-    echo $response->headers();
-    echo $response->body();
+    echo "Mensagem enviada com sucesso";
+  
     ?>
 
 </body>
